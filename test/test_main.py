4f2b58b0709e274794bc9969d1edb05f9efda048
Peter Osas <osaspeter6@gmail.com>
2025-10-03 15:30:59 +0100

    added course unit testing

--- test/test_main.py
+++ test/test_main.py
@@ -1,12 +1,7 @@
-
-
-from fastapi.testclient import TestClient
-from main import app
 from fastapi import status
 
-client = TestClient(app)
 
-def test_home():
+def test_home(client):
     response = client.get("/")
     assert response.status_code == status.HTTP_200_OK
     assert response.json() == {"msg":"EduTrack lite api"}
