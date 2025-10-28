b8fd338bfa63e94c8dd6688e74df597e73e0f894
Peter Osas <osaspeter6@gmail.com>
2025-10-28 17:17:03 +0100

    add a middleware

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from routes.user import user_router
 from routes.course import course_router
 from routes.enroll import course_enroll
-
+import logging
 app = FastAPI(title="EduTrack Lite API")
 
 
 
+@app.middleware("http")
+async def log_request(request:Request, call_next):
+    logging.info(f'Incoming request:{request.method} {request.url}')
+    response = await call_next(request)
+    logging.info(f'Request status: {response.status_code}')
+    return response
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s = %(levelname)s - %(message)s",
+    filename='app.log',
+    filemode="a"
+)
 
 
 @app.get("/", tags={"Home"})
 def home():
+    logging.info("Root endpoint accessed")
     return {"msg":"EduTrack lite api"}
 
 
